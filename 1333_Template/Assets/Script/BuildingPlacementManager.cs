f808a96ec9eeda215b15df1838fa63dcd317b827
GinSyuan <gin.jingsyuan@gmail.com>
2025-06-12 09:57:49 -0700

    Lab6-adjust
    When you place the building, there will be color tell you you can place it here or not

--- 1333_Template/Assets/Script/BuildingPlacementManager.cs
+++ 1333_Template/Assets/Script/BuildingPlacementManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingPlacementManager : MonoBehaviour
@@ -5,6 +6,8 @@ public class BuildingPlacementManager : MonoBehaviour
     [SerializeField] private GridManager gridManager;
     private Building ghost;
     private BuildingData selectedData;
+    private List<Renderer> _renderers;
+    private List<Material[]> _originalMaterials;
 
     public void SelectBuilding(BuildingData data)
     {
@@ -117,14 +120,55 @@ public class BuildingPlacementManager : MonoBehaviour
         ghost = null;
     }
 
-    
+
     private void SetGhostMaterialTransparent(Building b)
     {
-        
+        // find all renderers on the ghost
+        _renderers = new List<Renderer>(b.GetComponentsInChildren<Renderer>());
+        _originalMaterials = new List<Material[]>();
+
+        foreach (var r in _renderers)
+        {
+            // keep a copy of the original mats
+            _originalMaterials.Add(r.materials);
+
+            // make new instances so we don’t modify the prefab’s shared materials
+            var mats = new Material[r.materials.Length];
+            for (int i = 0; i < mats.Length; i++)
+            {
+                mats[i] = new Material(r.materials[i]);
+                // switch to a transparent mode (assumes Standard Shader)
+                mats[i].SetFloat("_Mode", 3);                   // 3 = Transparent
+                mats[i].SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                mats[i].SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                mats[i].SetInt("_ZWrite", 0);
+                mats[i].DisableKeyword("_ALPHATEST_ON");
+                mats[i].EnableKeyword("_ALPHABLEND_ON");
+                mats[i].DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                mats[i].renderQueue = 3000;
+                // start semi-opaque so you can still see the model
+                Color col = mats[i].color;
+                col.a = 0.5f;
+                mats[i].color = col;
+            }
+            // assign our new transparent materials
+            r.materials = mats;
+        }
     }
 
     private void SetGhostColor(Color c)
     {
-        
+        if (_renderers == null) return;
+
+        foreach (var r in _renderers)
+        {
+            var mats = r.materials;
+            for (int i = 0; i < mats.Length; i++)
+            {
+                Color col = mats[i].color;
+                mats[i].color = new Color(c.r, c.g, c.b, col.a); // keep the same alpha
+            }
+            r.materials = mats;
+        }
     }
 }
